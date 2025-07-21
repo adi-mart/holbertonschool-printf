c0851ace3a38b264775e4bde0324bedacf0b27d8
Moha-A1 <11469@holbertonstudents.com>
2025-07-21 14:05:28 +0200

    Add struct format for printf handling

--- main.h
+++ main.h
@@ -2,7 +2,23 @@
 #define MAIN_H
 
 #include <stdarg.h>
+
+/**
+ * struct format - Structure associant un spécificateur à une fonction
+ * @specifier: Le caractère de format (ex: 'c', 's', 'd', etc.)
+ * @func: Pointeur vers la fonction correspondante
+ */
+typedef struct format
+{
+	char specifier;
+	int (*func)(va_list args);
+} format_t;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_integer(va_list args);
 
 #endif
