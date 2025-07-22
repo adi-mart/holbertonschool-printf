6046da4132a0a1f322b0c49b0e3c7198b7816e4b
Moha-A1 <11469@holbertonstudents.com>
2025-07-22 18:51:26 +0200

    feat: Gère les spécificateurs %d et %i dans _printf
    Ajout de la logique pour imprimer les nombres entiers, y compris les négatifs, via les fonctions print_int et print_number.

--- func_print.c
+++ func_print.c
@@ -39,3 +39,50 @@ int print_prct(va_list args)
 	_putchar('%');
 	return (1);
 }
+
+
+/* preparation de la recursion, je declare avant */
+int print_number(int n);
+
+/**
+ * print_int - récupère un entier de la va_list et l'affiche
+ * @args: liste des arguments
+ * Return: nombre de caractères affichés
+ */
+int print_int(va_list args)
+{
+	int n;
+	 n = va_arg(args, int);
+	return (print_number(n));
+}
+
+
+/**
+ * print_number - affiche un entier en utilisant la récursion
+ * @n: le nombre à afficher
+ * Return: nombre de caractères affichés
+ */
+int print_number(int n)
+{
+	int count = 0;
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+		count += print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+	count++;
+
+	return (count);
+}
